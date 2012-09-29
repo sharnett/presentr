e3be26322c6302e238c8e6c13107ee25ebcacaa9
Sean Harnett <srharnett@172-30-28-6.dynapool.nyu.edu>
2012-09-29 18:19:04 -0400

    MASSIVE improvements

--- james.py
+++ james.py
@@ -6,9 +6,24 @@ def james(limit = 1, tag = 'james'):
     url = 'http://api.tumblr.com/v2/tagged?tag=%s&limit=%d&api_key=%s'
     return load(urlopen(url % (tag, limit, key)))
 
-def get_first_photo():
-    return james()['response'][0]['photos'][0]['original_size']['url']
+def get_photos(limit = 1):
+    pass
+    response = james(limit = limit)['response']
+    photos = []
+    for i in xrange(limit):
+        photo = response[i].get('photos', None)
+        if photo:
+            photos += [photo[0]['original_size']['url']]
+    return photos
+
+def get_captions(limit = 1):
+    return [james(limit = limit)['response'][i].get('caption', None) for i in xrange(limit)]
 
 if __name__ == '__main__':
-    print dumps(james(), sort_keys=True, indent=4)
-    print get_first_photo()
+    print dumps(james(limit=1), sort_keys=True, indent=4)
+    print 'PHOTOS'
+    for i, photo in enumerate(get_photos(limit = 10)):
+        print i+1, photo
+    print 'CAPTIONS'
+    for i, caption in enumerate(get_captions(limit = 10)):
+        print i+1, caption
