52dfbb0a653fe4161a6247a9befbb3c577a97ca8
Sean Harnett <srharnett@172-30-28-6.dynapool.nyu.edu>
2012-09-29 17:45:25 -0400

    can grab first photo

--- james.py
+++ james.py
@@ -1,12 +1,14 @@
 from urllib2 import urlopen
 from json import load, dumps
 
-key = 'XO7suEuSmJhFAZdhHJBjDYeaecyfVatJBKtOhFzG8AH0VTiArJ' # consumer key
-url = 'http://api.tumblr.com/v2/tagged?tag=%s&limit=%d&api_key=%s'
-limit = 1
-tag = 'james'
+def james(limit = 1, tag = 'james'):
+    key = 'XO7suEuSmJhFAZdhHJBjDYeaecyfVatJBKtOhFzG8AH0VTiArJ' # consumer key
+    url = 'http://api.tumblr.com/v2/tagged?tag=%s&limit=%d&api_key=%s'
+    return load(urlopen(url % (tag, limit, key)))
 
-james = load(urlopen(url % (tag, limit, key)))
+def get_first_photo():
+    return james()['response'][0]['photos'][0]['original_size']['url']
 
-print dumps(james, sort_keys=True, indent=4)
-
+if __name__ == '__main__':
+    print dumps(james(), sort_keys=True, indent=4)
+    print get_first_photo()
