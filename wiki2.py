1212030ad7f5446ef5f2693c73f26937f174888f
Nick Mykins <nick.mykins@gmail.com>
2012-09-29 23:09:54 -0400

    wiki2 gets article ID, removes <ref> tags

--- wiki2.py
+++ wiki2.py
@@ -1,5 +1,6 @@
 from urllib2 import urlopen
 from json import load,dumps
+from splitpgraph import splitpgraph
 import re
 
 #def getFacts(subject):
@@ -9,12 +10,17 @@ section = 1
 url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=%d&titles='+subject+'&format=json'
 article = load(urlopen(url % section))
 
-articleText = article['query']['pages']['30075']['revisions'][0]['*']
+articleID = article['query']['pages'].keys()
+articleText = article['query']['pages'][articleID[0]]['revisions'][0]['*']
 
-print articleText
+#print articleText
 unwiki = re.compile(r'\[\[(?:[^|\]]*\|)?([^\]]+)\]\]')
 newText = unwiki.sub(r'\1', articleText)
-print newText
+newText = re.sub('<ref.*/ref>','',newText)
 
+sentences = splitpgraph(newText)
+
+print newText
+print articleID
 
 
