2c2c05c2d99d53cb03e94e7ef012bb463db98b4a
Sean Harnett <srharnett@172-30-28-6.dynapool.nyu.edu>
2012-09-30 02:30:35 -0400

    made wiki friendlier for james' latex junk

--- wiki.py
+++ wiki.py
@@ -2,36 +2,42 @@ from urllib2 import urlopen
 from json import load,dumps
 from splitpgraph import splitpgraph
 import re
+from random import sample
 
-def getFacts(subject):
-	presentation = {}
+def getFacts(subject, num_titles=3, num_sentences=27):
+    presentation = {}
 
-	def getTitle(section, text):
-		regex1 = r"\=\=(.*?)\=\="
-		header = re.search(regex1, text)
-		return header.group(1)
+    def getTitle(section, text):
+        regex1 = r"\=\=(.*?)\=\="
+        header = re.search(regex1, text)
+        return header.group(1)
 
-	def getText(section):
-		url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=%d&titles='+subject+'&format=json'
-		article = load(urlopen(url % section))
+    def getText(section):
+        url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=%d&titles='+subject+'&format=json'
+        article = load(urlopen(url % section))
+        articleID = article['query']['pages'].keys()
+        articleText = article['query']['pages'][articleID[0]]['revisions'][0]['*']
+        #print articleText
+        unwiki = re.compile(r'\[\[(?:[^|\]]*\|)?([^\]]+)\]\]')
+        newText = unwiki.sub(r'\1', articleText)
+        newText = re.sub('<.*>','',newText)
+        newText = re.sub('\{\{.*\}\}','',newText)
+        newText = re.sub('\n',' ',newText)
+        newText = re.sub('\'*','',newText)
+        return newText
 
-		articleID = article['query']['pages'].keys()
-		articleText = article['query']['pages'][articleID[0]]['revisions'][0]['*']
+    n = num_sentences/num_titles
+    titles = [getTitle(i, getText(i)) for i in xrange(1, num_titles+1)]
+    text = []
+    for section in xrange(1, 6):
+        text += splitpgraph(getText(section))[2:]
+    text = sample(text, num_sentences)
 
-		#print articleText
-		unwiki = re.compile(r'\[\[(?:[^|\]]*\|)?([^\]]+)\]\]')
-		newText = unwiki.sub(r'\1', articleText)
-		newText = re.sub('<.*>','',newText)
-		newText = re.sub('\{\{.*\}\}','',newText)
-		newText = re.sub('\n',' ',newText)
-		newText = re.sub('\'*','',newText)
+    return titles, text
 
-		return newText
-
-	for section in xrange(1,5):
-		sentences = splitpgraph(getText(section))
-		presentation[getTitle(section, getText(section))] = sentences[2:6]
-		
-	return presentation
-
-print getFacts('tiger')
+if __name__ == '__main__':
+    titles, text = getFacts('tiger')
+    for i, title in enumerate(titles):
+        print i, title
+    for i, t in enumerate(text):
+        print i, t
