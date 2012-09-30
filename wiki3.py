de4cad46619d41d74fe521020b23e7e7c419c970
bananaradiation <bananaradiation@gmail.com>
2012-09-29 23:33:28 -0400

    getTitle section

---
+++ wiki3.py
@@ -0,0 +1,36 @@
+from urllib2 import urlopen
+from json import load,dumps
+from splitpgraph import splitpgraph
+import re
+
+#def getFacts(subject):
+subject = 'tiger'
+
+def getTitle(title, section, text):
+	regex1 = r"\=\=(.*?)\=\="
+	header = re.search(regex1, text)
+	title.append(header.group(1))
+	return title
+
+for section in xrange(1,5):
+	title = []
+
+	url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=%d&titles='+subject+'&format=json'
+	article = load(urlopen(url % section))
+
+	articleID = article['query']['pages'].keys()
+	articleText = article['query']['pages'][articleID[0]]['revisions'][0]['*']
+
+	#print articleText
+	unwiki = re.compile(r'\[\[(?:[^|\]]*\|)?([^\]]+)\]\]')
+	newText = unwiki.sub(r'\1', articleText)
+	newText = re.sub('<ref.*/ref>','',newText)
+	newText = re.sub('\{\{.*\}\}','',newText)
+	newText = re.sub('\n',' ',newText)
+
+	sentences = splitpgraph(newText)
+
+	print sentences[2]
+
+	print getTitle(title, section, newText)
+
