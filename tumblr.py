1e0d7a26efb09d763e9249346fa6e271ed834c34
Sean Harnett <srharnett@172-30-28-6.dynapool.nyu.edu>
2012-09-30 00:56:52 -0400

    some renamings

--- tumblr.py
+++ tumblr.py
@@ -6,23 +6,35 @@ def james(limit = 1, tag = 'james'):
     url = 'http://api.tumblr.com/v2/tagged?tag=%s&limit=%d&api_key=%s'
     return load(urlopen(url % (tag, limit, key)))
 
-def get_photos(response, limit = 1):
-    limit = min([len(response), limit])
-    photos = [None]*limit
-    for i in xrange(limit):
+def get_photos(response, limit=10):
+    n = len(response)
+    photos = [None]*n
+    for i in xrange(n):
         photo = response[i].get('photos', None)
         if photo:
             photos[i] = photo[0]['original_size']['url']
-    return clean(photos)
+    return clean(photos)[:limit]
 
-def get_captions(response, limit = 1):
-    limit = min([len(response), limit])
-    captions = [response[i].get('caption', None) for i in xrange(limit)]
-    return clean(captions)
+def get_captions(response, limit=10):
+    captions = [r.get('caption', None) for r in response]
+    return clean(captions)[:limit]
 
 def clean(a):
     ''' returns same list without empty elements '''
     return [x for x in a if x]
+
+if __name__ == '__main__':
+    response = james(limit=20, tag='tiger')['response']
+    #print dumps(response[0], sort_keys=True, indent=4)
+    print 'PHOTOS'
+    photos = get_photos(response)
+    captions = get_captions(response)
+    for i, photo in enumerate(photos):
+        print i+1, photo
+    print 'CAPTIONS'
+    for i, caption in enumerate(captions):
+        print i+1, caption
+    #make_html(photos, captions)
 
 def make_html(photos, captions):
     f = open('index.html', 'w')
@@ -33,18 +45,3 @@ def make_html(photos, captions):
         f.write('<img src="{}" width=200 height=100>\n<br \>'.format(photo))
     f.write('</body></html>')
     f.close()
-
-if __name__ == '__main__':
-    response = james(limit=10, tag='tiger')['response']
-    print dumps(response[0], sort_keys=True, indent=4)
-    print 'PHOTOS'
-    photos = get_photos(response, limit=10)
-    captions = get_captions(response, limit=10)
-    for i, photo in enumerate(photos):
-        print i+1, photo
-    print 'CAPTIONS'
-    for i, caption in enumerate(captions):
-        print i+1, caption
-    make_html(photos, captions)
-
-
--- wiki.py
+++ wiki.py
@@ -1,11 +1,37 @@
 from urllib2 import urlopen
 from json import load,dumps
+from splitpgraph import splitpgraph
+import re
 
-#def getFacts(subject):
-subject = 'pizza'
+def getFacts(subject):
+	presentation = {}
 
-url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=1&titles='+subject+'&format=json'
-article = load(urlopen(url))
+	def getTitle(section, text):
+		regex1 = r"\=\=(.*?)\=\="
+		header = re.search(regex1, text)
+		return header.group(1)
 
-articleText = article['query']['pages']['24768']['revisions'][0]['*']
-print articleText
+	def getText(section):
+		url = 'http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&rvsection=%d&titles='+subject+'&format=json'
+		article = load(urlopen(url % section))
+
+		articleID = article['query']['pages'].keys()
+		articleText = article['query']['pages'][articleID[0]]['revisions'][0]['*']
+
+		#print articleText
+		unwiki = re.compile(r'\[\[(?:[^|\]]*\|)?([^\]]+)\]\]')
+		newText = unwiki.sub(r'\1', articleText)
+		newText = re.sub('<.*>','',newText)
+		newText = re.sub('\{\{.*\}\}','',newText)
+		newText = re.sub('\n',' ',newText)
+		newText = re.sub('\'*','',newText)
+
+		return newText
+
+	for section in xrange(1,5):
+		sentences = splitpgraph(getText(section))
+		presentation[getTitle(section, getText(section))] = sentences[2:6]
+		
+	return presentation
+
+print getFacts('tiger')
