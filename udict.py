863757d041b5bbb101edde1b9da461a6894a2863
Sean Harnett <srharnett@gmail.com>
2016-04-17 00:15:15 -0400

    updated for change in urban dictionary api

--- udict.py
+++ udict.py
@@ -1,28 +1,16 @@
-from urllib2 import urlopen
-from BeautifulSoup import BeautifulSoup
-import re
+import requests
 
 def get_definitions(tag):
-	tag = re.sub(' ','+', tag) # put input into urban form: 'new york' --> 'new+york'
-	url = 'http://www.urbandictionary.com/define.php?term=%s'
-	data = urlopen(url % (tag)).read()
-	data = BeautifulSoup(data)
-
-	nodefinition, k, i = 0, 0, 0
-	maxdefn = len(data.findAll('div', attrs={'class' : 'definition'}))
+	tag = tag.replace(' ', '+')
+	url = 'http://api.urbandictionary.com/v0/define?term=%s'
+	result = requests.get(url % tag)
 	defns = []
 
-	while nodefinition < 3 and k < maxdefn:
-		defn = data.findAll('div', attrs={'class' : 'definition'})[k].text	
-		if defn.find('.',5)>0:
-		        defn = re.sub('&quot;', '\"', defn)     # remove awkward urban dict format
-                        defn = re.sub('\d', '', defn)           # remove numbers
-                	defn = re.sub('\A. ', '', defn)         # remove bad formatting at beginning
-			defn = defn[0:defn.find('.',5)+1]
-			defns += [defn.capitalize()]
-			nodefinition += 1
-			i += 1
-		k += 1
+	for i in range(3):
+		try:
+			defns.append(result.json()['list'][i]['definition'])
+		except IndexError, KeyError:
+			break
 
 	# padding:
 	if len(defns) == 2:
@@ -39,3 +27,5 @@ def get_definitions(tag):
 if __name__ == '__main__':
 	tag = raw_input('topic: ')	
 	definitions = get_definitions(tag)
+	for d in definitions:
+		print(d)
